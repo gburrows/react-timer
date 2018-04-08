85a73b5823d10e52e77749e3890e173dc50026ea
Graham Burrows <g.burrows85@gmail.com>
2018-04-08 16:39:19 +0100

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+	return (
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="dropdown menu" data-dropdown-menu>
+					<li className="menu-text">React Timer App</li>
+					<li>
+						<IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+					</li>
+					<li>
+						<Link to="/" activeClassName="active-link">Countdown</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+				<ul className="menu">
+					<li className="menu-text">Created by <a href="https://github.com/gburrows" target="_blank">Graham Burrows</a></li>
+				</ul>
+			</div>
+		</div>
+	)
+};
+
+module.exports = Nav;
